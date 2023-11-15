7f5f854c06e0d8725e5ec9c7cc8ac15ff99634af
Abdellatif Bouyatas <abouyata@c3r8p2.1337.ma>
2023-11-15 03:14:16 +0100

    main_test

--- src/ft_lstiter_bonus.c
+++ src/ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abouyata <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 20:46:49 by abouyata          #+#    #+#             */
-/*   Updated: 2023/11/14 20:48:29 by abouyata         ###   ########.fr       */
+/*   Updated: 2023/11/15 00:16:06 by abouyata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,16 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst->next;
 	}
 }
+/*#include <stdio.h>
+void my_function(void *lst)
+{
+	char *s = lst;
+	printf("%s|",s);
+}
+int main()
+{
+	t_list *head = ft_lstnew("abdellatif");
+	t_list *new  = ft_lstnew("Bouyatas");
+	head->next = new;
+	ft_lstiter(head, my_function);
+}*/
